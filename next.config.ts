b987b0bb6c0819e41e7fadea3b1275ba32b49530
Ruth Moraa <moraar976@gmail.com>
2026-04-21 19:31:27 +0300

    new changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['lucide-react', 'zustand'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './',
+    };
+    return config;
+  },
   turbopack: {
     resolveAlias: {
       '@': './',
